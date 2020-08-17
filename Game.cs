f6df6dd0f67932c5d5acfd52dfc8687a6dab1269
Joseph Pena <jgpena2888@gmail.com>
2020-08-17 16:27:15 -0500

    Added player stats. printed player health

--- Game.cs
+++ Game.cs
@@ -8,7 +8,15 @@ namespace HelloWorld
     {
         public void Run()
         {
-
+            float health = 100.0f;
+            float healthRegen = 20;
+            Console.WriteLine("You there, what is your name?");
+            string name = Console.ReadLine();
+            bool ready = true;
+            int level = 1;
+            Console.WriteLine(health);
+            health = health + healthRegen;
+            Console.Write(name + " healed " + healthRegen + " health");
         }
     }
 }
