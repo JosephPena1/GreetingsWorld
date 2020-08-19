14cf6bc9ee2776a15c63309c142961f7913d84db
Joseph Pena <jgpena2888@gmail.com>
2020-08-19 11:45:07 -0500

    fixed some lines, and added more user inputs

--- Game.cs
+++ Game.cs
@@ -9,14 +9,22 @@ namespace HelloWorld
         public void Run()
         {
             float health = 100.0f;
-            float healthRegen = 20;
-            Console.WriteLine("You there, what is your name?");
-            string name = Console.ReadLine();
+            float healthRegen = 20.0f;
             bool ready = true;
             int level = 1;
-            Console.WriteLine(health);
-            health = health + healthRegen;
-            Console.Write(name + " healed " + healthRegen + " health");
+            //^stats^\\
+            Console.WriteLine("You there, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + ". That's a good name.");
+            Console.WriteLine("So " + name +", what is your role?");
+            string role = Console.ReadLine();
+            Console.WriteLine("Do you have a companion? What is their name?");
+            string compn = Console.ReadLine();
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Role: " + role);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("Your companion " + compn + " is waiting.");
         }
     }
 }
